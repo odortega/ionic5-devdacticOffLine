d5231323b359a27e3723faec081543e33b336aea
odortega <odortega@gmail.com>
2020-03-17 01:46:12 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { NetworkService, ConnectionStatus } from './services/network.service';
+import { OfflineManagerService } from './services/offline-manager.service';
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent {
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    private offlineManager: OfflineManagerService,
+    private networkService: NetworkService
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+      this.networkService.onNetworkChange().subscribe((status: ConnectionStatus) => {
+        if (status == ConnectionStatus.Online) {
+          this.offlineManager.checkForEvents().subscribe();
+        }
+      });
+    });
+  }
+}
